d7afd47c2ec6c25b080cf3d8497ddc0d2a21a72a
AlexMa77 <am9614843@gmail.com>
2025-10-16 09:59:16 -0500

    Clase 2025-10-16

---
+++ modulo2/javascript/03_operadores_aritmeticos.js
@@ -0,0 +1,45 @@
+console.log("OPERADORES ARITMETICOS");
+
+numero1 = 20;
+numero2 = 5;
+
+console.log("Suma");
+suma= numero1 + numero2;
+console.log("Resultado de la suma es:",suma);
+
+console.log("Resta");
+resta= numero1 - numero2;
+console.log("Resultado de la resta es:",resta);
+
+console.log("Multiplicacion");
+multiplicacion= numero1 * numero2;
+console.log("Resultado de la multiplicacion es:",multiplicacion);
+
+console.log("Division");
+division= numero1 / numero2;
+console.log("Resultado de la division es:",division);
+
+console.log("Modulo");
+modulo= numero1 % numero2;
+console.log("Resultado del modulo es:",modulo);
+
+console.log("Potencia");
+potencia= numero1 ** numero2;
+console.log("Resultado de la potencia es:",potencia);
+
+//Operadores de Comparacion
+console.log("Operadores de Comparacion");
+console.log("Igualdad debil ==", "5"==5);
+console.log("Igualdad estricta ===", "5"===5);
+console.log("Desigualdad debil !=", "5"!=5);
+console.log("Desigualdad estricta !==", "5"!==5);
+console.log("Mayor que >", "5">5);
+console.log("Menor que <", "5"<5);
+console.log("Mayor o igual que >=", "5">=5);
+console.log("Menor o igual que <=", "5"<=5);
+
+//Operadores Logicos
+console.log("Operadores Logicos");
+console.log("AND &&", "5"<=5 && "5"===5);
+console.log("OR ||", "5"<=5 || "6"===4);
+console.log("NEGACION !", !(5==5));
