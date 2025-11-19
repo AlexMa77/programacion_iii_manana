92fc0fbd7ec8f89b07fbedd92779058cfb8087ae
AlexMa77 <am9614843@gmail.com>
2025-11-18 20:31:08 -0500

    Clase 2025-11-18

--- modulo3/nestjs/blog-backend/dist/src/app.module.js
+++ modulo3/nestjs/blog-backend/dist/src/app.module.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const basics_module_1 = require("./basics/basics.module");
@@ -16,7 +17,19 @@ let AppModule = class AppModule {
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [basics_module_1.BasicsModule],
+        imports: [
+            basics_module_1.BasicsModule,
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'postgres',
+                host: 'localhost',
+                port: 5432,
+                username: 'postgres',
+                password: '12345',
+                database: 'blogdb',
+                autoLoadEntities: true,
+                synchronize: true,
+            }),
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
--- modulo3/nestjs/blog-backend/src/app.module.ts
+++ modulo3/nestjs/blog-backend/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BasicsModule } from './basics/basics.module';
 
 @Module({
-  imports: [BasicsModule],
+  imports: [
+    BasicsModule,
+
+    // 🔥 Conexión a PostgreSQL
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',  
+      password: '12345',  
+      database: 'blogdb',       
+      autoLoadEntities: true,
+      synchronize: true,        
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
