f61f04c6c979ee165bdebc51c7fcb15d83d660e7
AlexMa77 <am9614843@gmail.com>
2025-12-04 10:21:13 -0500

    Clase 2025-12-04

---
+++ modulo3/nestjs/blog-backend/dist/mail/mail.controller.d.ts
@@ -0,0 +1,10 @@
+import { MailService } from './mail.service';
+import { SendMailDto } from './dto/send-mail.dto';
+import { SuccessResponseDto } from '../common/dto/response.dto';
+export declare class MailController {
+    private readonly mailService;
+    constructor(mailService: MailService);
+    sendGmail(dto: SendMailDto): Promise<SuccessResponseDto<{
+        messageId: any;
+    }>>;
+}
